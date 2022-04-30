f432845b8c6e3cb11b4648c1818b72d86eda9012
Ahmed22 <Elkhami.it@gmail.com>
2022-04-30 23:03:45 +0200

    *Mocking repository with mockk library in GetFoodCategoriesUseCaseTest.kt. *Adding mockk dependency.

--- app/src/test/java/com/ellkhamitech/foodswipe/domain/use_case/GetFoodCategoriesUseCaseTest.kt
+++ app/src/test/java/com/ellkhamitech/foodswipe/domain/use_case/GetFoodCategoriesUseCaseTest.kt
@@ -1,38 +1,28 @@
 package com.ellkhamitech.foodswipe.domain.use_case
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.ellkhamitech.foodswipe.data.repository.FoodRepositoryImpl
 import com.ellkhamitech.foodswipe.data.stub.FoodCategoriesStub
 import com.ellkhamitech.foodswipe.domain.model.FoodCategory
 import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestRule
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.junit.MockitoJUnitRunner
 
 /**
  * Created by A.Elkhami on 21,April,2022
  */
 @ExperimentalCoroutinesApi
-@RunWith(MockitoJUnitRunner::class)
 class GetFoodCategoriesUseCaseTest {
 
-    @get:Rule
-    val testRule: TestRule = InstantTaskExecutorRule()
-
     private lateinit var getFoodCategoriesUseCase: GetFoodCategoriesUseCase
-
-    @Mock
     private lateinit var foodRepository: FoodRepositoryImpl
 
     @Before
     fun setup() {
+        foodRepository = mockk()
         getFoodCategoriesUseCase = GetFoodCategoriesUseCase(foodRepository)
     }
 
@@ -40,7 +30,7 @@ class GetFoodCategoriesUseCaseTest {
     fun `when getFoodCategoriesUseCase called, expect data`() = runTest {
         val success = Result.success(FoodCategoriesStub.foodCategories)
 
-        `when`(foodRepository.getFoodCategories()).thenReturn(success)
+        coEvery { foodRepository.getFoodCategories() } returns success
 
         val foodCategories = getFoodCategoriesUseCase()
 
@@ -51,7 +41,7 @@ class GetFoodCategoriesUseCaseTest {
     fun `when getFoodCategoriesUseCase called, expect error`() = runTest {
         val failure = Result.failure<List<FoodCategory>>(Throwable(message = "error"))
 
-        `when`(foodRepository.getFoodCategories()).thenReturn(failure)
+        coEvery { foodRepository.getFoodCategories() } returns failure
 
         val foodCategories = getFoodCategoriesUseCase()
 
