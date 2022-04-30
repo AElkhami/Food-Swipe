3451d90763b5b4eedf4a4fc10278f5ab1fd7c366
Ahmed22 <Elkhami.it@gmail.com>
2022-04-30 16:53:02 +0200

    *Refactoring GetFoodCategoriesUseCaseTest.kt to use mockito to mock the dependencies. *Adding mockito dependencies. *Removing unused TestRule from MapImageUrlUseCaseTest.kt

--- app/src/test/java/com/ellkhamitech/foodswipe/domain/use_case/GetFoodCategoriesUseCaseTest.kt
+++ app/src/test/java/com/ellkhamitech/foodswipe/domain/use_case/GetFoodCategoriesUseCaseTest.kt
@@ -1,7 +1,9 @@
 package com.ellkhamitech.foodswipe.domain.use_case
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.ellkhamitech.foodswipe.data.repository.fake.FoodRepositoryImplFake
+import com.ellkhamitech.foodswipe.data.repository.FoodRepositoryImpl
+import com.ellkhamitech.foodswipe.data.stub.FoodCategoriesStub
+import com.ellkhamitech.foodswipe.domain.model.FoodCategory
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -9,38 +11,49 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
 /**
  * Created by A.Elkhami on 21,April,2022
  */
 @ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class GetFoodCategoriesUseCaseTest {
 
     @get:Rule
     val testRule: TestRule = InstantTaskExecutorRule()
 
     private lateinit var getFoodCategoriesUseCase: GetFoodCategoriesUseCase
-    private lateinit var foodRepository: FoodRepositoryImplFake
+
+    @Mock
+    private lateinit var foodRepository: FoodRepositoryImpl
 
     @Before
     fun setup() {
-        foodRepository = FoodRepositoryImplFake()
         getFoodCategoriesUseCase = GetFoodCategoriesUseCase(foodRepository)
     }
 
     @Test
     fun `when getFoodCategoriesUseCase called, expect data`() = runTest {
+        val success = Result.success(FoodCategoriesStub.foodCategories)
+        `when`(foodRepository.getFoodCategories()).thenReturn(success)
+
         val foodCategories = getFoodCategoriesUseCase()
 
-        assertThat(foodCategories).isEqualTo(foodRepository.successCase)
+        assertThat(foodCategories).isEqualTo(success)
     }
 
     @Test
     fun `when getFoodCategoriesUseCase called, expect error`() = runTest {
-        foodRepository.setReturnError(true)
+        val failure = Result.failure<List<FoodCategory>>(Throwable(message = "error"))
+
+        `when`(foodRepository.getFoodCategories()).thenReturn(failure)
 
         val foodCategories = getFoodCategoriesUseCase()
 
-        assertThat(foodCategories).isEqualTo(foodRepository.failureCase)
+        assertThat(foodCategories).isEqualTo(failure)
     }
 }
--- app/src/test/java/com/ellkhamitech/foodswipe/domain/use_case/MapImageUrlUseCaseTest.kt
+++ app/src/test/java/com/ellkhamitech/foodswipe/domain/use_case/MapImageUrlUseCaseTest.kt
@@ -1,21 +1,15 @@
 package com.ellkhamitech.foodswipe.domain.use_case
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.ellkhamitech.foodswipe.data.model.Category
 import com.ellkhamitech.foodswipe.data.stub.FoodCategoriesStub
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestRule
 
 /**
  * Created by A.Elkhami on 21,April,2022
  */
 class MapImageUrlUseCaseTest {
-
-    @get:Rule
-    val testRule: TestRule = InstantTaskExecutorRule()
 
     private lateinit var mapImageUrlUseCase: MapImageUrlUseCase
 
